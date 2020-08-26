bd3216c4d05d03af78f212b521e823e7208ec834
Himanshu <himanshu1988mishra@gmail.com>
2020-08-27 00:29:27 +0530

    Updates

---
+++ precache-manifest.1e286d21fbc6b133a3f128895aeb47e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e44a18de5b55f55eb18f79869748a38b",
+    "url": "/spaceXdata/index.html"
+  },
+  {
+    "revision": "e5b391ccc1716a774c54",
+    "url": "/spaceXdata/static/css/main.79a0e59d.chunk.css"
+  },
+  {
+    "revision": "2e2d5d36adf27fe3713b",
+    "url": "/spaceXdata/static/js/2.8a76e984.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/spaceXdata/static/js/2.8a76e984.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5b391ccc1716a774c54",
+    "url": "/spaceXdata/static/js/main.03e399be.chunk.js"
+  },
+  {
+    "revision": "7cd572a7f637f38aaca6",
+    "url": "/spaceXdata/static/js/runtime-main.507f9b3f.js"
+  }
+]);
